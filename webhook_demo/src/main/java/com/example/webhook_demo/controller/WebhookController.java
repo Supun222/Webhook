4547da5ef224d61220c9a0012dc405833cff6d25
Supun222 <supunnisala12345@gmail.com>
2022-09-15 12:08:36 +0530

    second commit

--- webhook_demo/src/main/java/com/example/webhook_demo/controller/WebhookController.java
+++ webhook_demo/src/main/java/com/example/webhook_demo/controller/WebhookController.java
@@ -2,17 +2,15 @@ package com.example.webhook_demo.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin("https://localhost:8080")
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/webhook")
 public class WebhookController {
 
 
-    @PostMapping("/webhook") // http://localhost:8080/api/webhook
+    @PostMapping // http://localhost:8080/api/webhook
     public ResponseEntity<String> print(@RequestBody String requestBody){
         System.out.println("#### Webhook ###" + requestBody);
         return new ResponseEntity<String>(requestBody, HttpStatus.OK);
