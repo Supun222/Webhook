f28f66c0f995d755f61987c7a2a3b8fbb905ca75
Supun222 <supunnisala12345@gmail.com>
2022-09-20 09:44:25 +0530

    Some changessssss

--- webhook_demo/src/main/java/com/example/webhook_demo/controller/WebhookController.java
+++ webhook_demo/src/main/java/com/example/webhook_demo/controller/WebhookController.java
@@ -11,6 +11,6 @@ public class WebhookController {
 
     @PostMapping // http://localhost:8080/api/webhook
     public ResponseEntity<String> print(@RequestBody String requestBody){
-        System.out.println("########## Hi webhook is working  #######" + requestBody);
+        System.out.println("########### Hi webhook is working  #######" + requestBody);
         return new ResponseEntity<String>(requestBody, HttpStatus.OK);
     }}
